90752d5d4fbb7532ec4da0a7843b86bec4d50f1a
Dan Levy <dan@danlevy.net>
2022-05-23 14:25:28 -0600

    Add lint config based on standardjs+eslint overrides

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+/**
+ * @type {import('@typescript-eslint/utils').TSESLint.Linter.Config}
+ */
+const config = {
+  extends: 'standard-with-typescript',
+
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'only-multiline',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', 'never'],
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/restrict-template-expressions': [
+      'warn',
+      {
+        allowAny: true,
+        allowNumber: true,
+        allowBoolean: true,
+        allowNullish: true,
+        allowRegExp: true,
+      },
+    ],
+    // note you must disable the base rule as it can report incorrect errors
+    quotes: 'off',
+    '@typescript-eslint/quotes': ['error', 'single', { avoidEscape: true }],
+  },
+  '@typescript-eslint/no-misused-promises': [
+    'error',
+    {
+      checksConditionals: true,
+      checksVoidReturn: true,
+    },
+  ],
+};
+
+module.exports = config;
