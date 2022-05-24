f87516648f0830495ff73d75f1b180e6862d9c48
Dan Levy <dan@danlevy.net>
2022-05-24 10:06:35 -0600

    Fix for browsers & v1.4.2 bump

--- src/operators.ts
+++
@@ -1,6 +0,0 @@
-
-export const AssignmentOperators: { [key: string]: (a: any, b: any) => any } = {
-  '%=': (a: number, b: number) => a % b,
-  '||=': (a: any, b: any) => a || b,
-  '??=': (a: any, b: any) => a ?? b,
-};
