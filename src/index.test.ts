0e70ae09e99fd1f091fc65f79785d91644840c5c
Dan Levy <397632+justsml@users.noreply.github.com>
2022-06-13 20:48:58 +0000

    Updated formatting & TS version

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,30 +1,30 @@
-import { ruleFactory } from "./index";
-import mockDateHelper from "./utils/mockDateHelper";
+import { ruleFactory } from './index';
+import mockDateHelper from './utils/mockDateHelper';
 
 const omitRuntime = ({ runTime, startTime, ...keys }: any) => keys;
 
-describe("Assignment Operators", () => {
-  test("should be able to assign a value to a variable", () => {
-    const rulesFn = ruleFactory("amITheWalrus = true");
+describe('Assignment Operators', () => {
+  test('should be able to assign a value to a variable', () => {
+    const rulesFn = ruleFactory('amITheWalrus = true');
     expect(rulesFn({})).toEqual({ amITheWalrus: true });
   });
-  test("and return a variable by name", () => {
+  test('and return a variable by name', () => {
     const rulesFn = ruleFactory([
-      "amITheWalrus = foo + bar",
-      { return: "amITheWalrus" },
+      'amITheWalrus = foo + bar',
+      { return: 'amITheWalrus' },
     ]);
     expect(rulesFn({ foo: 1, bar: 2 })).toBe(3);
   });
 });
 
-describe("Logical", () => {
-  describe("if/then/else", () => {
+describe('Logical', () => {
+  describe('if/then/else', () => {
     test("can process 'then' rules", () => {
       const rulesFn = ruleFactory(
         [
-          { if: "price >= 25", then: "discount = 5" },
-          { if: "price >= 100", then: "discount = 20" },
-          { return: "discount" },
+          { if: 'price >= 25', then: 'discount = 5' },
+          { if: 'price >= 100', then: 'discount = 20' },
+          { return: 'discount' },
         ],
         { trace: true }
       );
@@ -39,7 +39,7 @@ describe("Logical", () => {
 
     test("can process omitted 'else' rules", () => {
       const rulesFn = ruleFactory(
-        [{ if: "price >= 100", then: "discount = 20" }, { return: "discount" }],
+        [{ if: 'price >= 100', then: 'discount = 20' }, { return: 'discount' }],
         { trace: true }
       );
 
@@ -50,18 +50,18 @@ describe("Logical", () => {
       expect(result.returnValue).toBe(undefined);
     });
 
-    test("can process series of if/then rules", () => {
+    test('can process series of if/then rules', () => {
       const rulesFn = ruleFactory(
         [
-          { if: 'user.plan == "premium"', then: "discount = 15" },
-          { if: "user.employee == true", then: "discount = 15" },
-          { return: "discount" },
+          { if: 'user.plan == "premium"', then: 'discount = 15' },
+          { if: 'user.employee == true', then: 'discount = 15' },
+          { return: 'discount' },
         ],
         { trace: true }
       );
       const result = rulesFn({
         user: {
-          plan: "premium",
+          plan: 'premium',
           employee: true,
         },
         config: {
@@ -78,13 +78,13 @@ describe("Logical", () => {
     });
   });
 
-  describe("and/or", () => {
+  describe('and/or', () => {
     test("can process 'and' rules", () => {
       const rulesFn = ruleFactory(
         [
-          { if: { and: ["price >= 25", "price <= 50"] }, then: "discount = 5" },
-          { if: "price >= 100", then: "discount = 20" },
-          { return: "discount" },
+          { if: { and: ['price >= 25', 'price <= 50'] }, then: 'discount = 5' },
+          { if: 'price >= 100', then: 'discount = 20' },
+          { return: 'discount' },
         ],
         { trace: true }
       );
@@ -99,12 +99,12 @@ describe("Logical", () => {
     test("can process 'or' rules", () => {
       const rulesFn = ruleFactory(
         [
-          { if: "price <= 100", then: "discount = 5" },
+          { if: 'price <= 100', then: 'discount = 5' },
           {
-            if: { or: ["price >= 100", "user.isAdmin == true"] },
-            then: "discount = 20",
+            if: { or: ['price >= 100', 'user.isAdmin == true'] },
+            then: 'discount = 20',
           },
-          { return: "discount" },
+          { return: 'discount' },
         ],
         { trace: true }
       );
@@ -115,18 +115,18 @@ describe("Logical", () => {
       expect(result.returnValue).toBe(20);
       expect(result.input.discount).toBe(20);
     });
-    test("can process nested logical rule arrays", () => {
+    test('can process nested logical rule arrays', () => {
       const rulesFn = ruleFactory(
         [
           {
-            if: "price <= 100",
-            then: ["discount = 5", "user.discountApplied = true"],
+            if: 'price <= 100',
+            then: ['discount = 5', 'user.discountApplied = true'],
           },
           {
-            if: { and: ["price >= 90", "user.discountApplied != true"] },
-            then: "discount = 20",
+            if: { and: ['price >= 90', 'user.discountApplied != true'] },
+            then: 'discount = 20',
           },
-          { return: "discount" },
+          { return: 'discount' },
         ],
         { trace: true }
       );
@@ -141,40 +141,40 @@ describe("Logical", () => {
   });
 });
 
-describe("Custom Functions", () => {
-  test("can use functions as expressions", () => {
-    const unMockDate = mockDateHelper(new Date("2020-01-20T00:00:00.000Z"));
+describe('Custom Functions', () => {
+  test('can use functions as expressions', () => {
+    const unMockDate = mockDateHelper(new Date('2020-01-20T00:00:00.000Z'));
     const rulesFn = ruleFactory('DATEISO("10m")');
     expect(rulesFn({})).toMatchSnapshot();
     unMockDate();
   });
 
-  test("can invoke DATEISO function", () => {
-    const unMockDate = mockDateHelper(new Date("2020-01-20T00:00:00.000Z"));
+  test('can invoke DATEISO function', () => {
+    const unMockDate = mockDateHelper(new Date('2020-01-20T00:00:00.000Z'));
     const rulesFn = ruleFactory(
       [
-        { if: "3 >= 1", then: 'inTenMinutes = DATEISO("10m")' },
-        { return: "inTenMinutes" },
+        { if: '3 >= 1', then: 'inTenMinutes = DATEISO("10m")' },
+        { return: 'inTenMinutes' },
       ],
       { trace: false }
     );
 
-    const input = { addToDate: "10m" };
+    const input = { addToDate: '10m' };
     const result = rulesFn(input);
 
     expect(result).toMatch(/.*\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.*/);
     unMockDate();
   });
 
-  test("can invoke DATE function", () => {
+  test('can invoke DATE function', () => {
     const rulesFn = ruleFactory(
       [
         'today = DATE(DATEISO("0d"))',
         'tomorrow = DATE(DATEISO("1d"))',
         {
-          if: "tomorrow > today",
-          then: "return tomorrow - today",
-          else: "return 0",
+          if: 'tomorrow > today',
+          then: 'return tomorrow - today',
+          else: 'return 0',
         },
       ],
       { trace: false }
@@ -185,32 +185,32 @@ describe("Custom Functions", () => {
     expect(isOneDay).toBe(true);
   });
 
-  test("can use functional array helpers", () => {
+  test('can use functional array helpers', () => {
     const rulesFn = ruleFactory(
       [
         'availableCities = FILTER_VALUES(["London", "Milan"], cities)',
-        { return: "availableCities" },
+        { return: 'availableCities' },
       ],
       { trace: true }
     );
 
     const input = {
-      cities: ["Denver", "London", "LA"],
+      cities: ['Denver', 'London', 'LA'],
       onlyUSA: true,
     };
     const result = rulesFn(input);
 
     expect(result.trace.map(omitRuntime)).toMatchSnapshot();
-    expect(result.returnValue).toStrictEqual(["Denver", "LA"]);
+    expect(result.returnValue).toStrictEqual(['Denver', 'LA']);
   });
 
-  test("can use CONTAINS array function", () => {
+  test('can use CONTAINS array function', () => {
     const rulesFn = ruleFactory(['hasLondon = CONTAINS("London", cities)'], {
       trace: true,
     });
 
     const input = {
-      cities: ["Denver", "London", "LA"],
+      cities: ['Denver', 'London', 'LA'],
       onlyUSA: true,
     };
     const result = rulesFn(input);
@@ -220,62 +220,62 @@ describe("Custom Functions", () => {
   });
 });
 
-describe("Assignment Operators", () => {
-  test("can process increment operator +=", () => {
+describe('Assignment Operators', () => {
+  test('can process increment operator +=', () => {
     const rulesFn = ruleFactory([
-      { if: "price >= 100", then: "discount += 20" },
-      { return: "discount" },
+      { if: 'price >= 100', then: 'discount += 20' },
+      { return: 'discount' },
     ]);
 
     expect(rulesFn({ price: 100, discount: 10 })).toBe(30);
   });
-  test("minus equals: -=", () => {
+  test('minus equals: -=', () => {
     const rulesFn = ruleFactory([
-      { if: "price >= 100", then: "discount -= 5" },
-      { return: "discount" },
+      { if: 'price >= 100', then: 'discount -= 5' },
+      { return: 'discount' },
     ]);
 
     expect(rulesFn({ price: 100, discount: 10 })).toBe(5);
   });
 
-  test("times equals: *=", () => {
+  test('times equals: *=', () => {
     const rulesFn = ruleFactory([
-      { if: "price >= 100", then: "discount *= 5" },
-      { return: "discount" },
+      { if: 'price >= 100', then: 'discount *= 5' },
+      { return: 'discount' },
     ]);
     expect(rulesFn({ price: 100, discount: 10 })).toBe(50);
   });
-  test("divided by equals: /=", () => {
+  test('divided by equals: /=', () => {
     const rulesFn = ruleFactory([
-      { if: "price >= 100", then: "discount /= 5" },
-      { return: "discount" },
+      { if: 'price >= 100', then: 'discount /= 5' },
+      { return: 'discount' },
     ]);
     expect(rulesFn({ price: 100, discount: 10 })).toBe(2);
   });
-  test("nullish coalescing null: ??=", () => {
+  test('nullish coalescing null: ??=', () => {
     const rulesFn = ruleFactory([
-      { if: "price >= 100", then: "discount ??= 13" },
-      { return: "discount" },
+      { if: 'price >= 100', then: 'discount ??= 13' },
+      { return: 'discount' },
     ]);
     expect(rulesFn({ price: 100, discount: null })).toBe(13);
   });
 
-  test("nullish coalescing falsy: ??=", () => {
+  test('nullish coalescing falsy: ??=', () => {
     const rulesFn = ruleFactory([
-      { if: "price >= 100", then: "discount ??= 13" },
-      { return: "discount" },
+      { if: 'price >= 100', then: 'discount ??= 13' },
+      { return: 'discount' },
     ]);
     expect(rulesFn({ price: 100, discount: 0 })).toBe(0);
   });
 });
 
-describe("Nested Rule Structures", () => {
-  test("can process complex rule expressions", () => {
+describe('Nested Rule Structures', () => {
+  test('can process complex rule expressions', () => {
     const rulesFn = ruleFactory(
       [
-        { if: "price >= 25", then: "discount = 5 * 2" },
-        { if: "price >= 100", then: "discount = 20 * 4" },
-        { return: "discount" },
+        { if: 'price >= 25', then: 'discount = 5 * 2' },
+        { if: 'price >= 100', then: 'discount = 20 * 4' },
+        { return: 'discount' },
       ],
       { trace: true }
     );
@@ -289,8 +289,8 @@ describe("Nested Rule Structures", () => {
   });
 });
 
-describe("can use functional object helpers", () => {
-  test("OBJECT_CONTAINS", () => {
+describe('can use functional object helpers', () => {
+  test('OBJECT_CONTAINS', () => {
     const rulesFn = ruleFactory('return OBJECT_CONTAINS("London", cities)');
 
     const hasLondonInput = {
@@ -304,8 +304,8 @@ describe("can use functional object helpers", () => {
     expect(rulesFn(hasTaipeiInput)).toBe(false);
   });
 
-  test("COUNT_KEYS", () => {
-    const rulesFn = ruleFactory("return COUNT_KEYS(cities)");
+  test('COUNT_KEYS', () => {
+    const rulesFn = ruleFactory('return COUNT_KEYS(cities)');
     const input = {
       cities: { Denver: true, London: true, LA: true },
     };
@@ -313,7 +313,7 @@ describe("can use functional object helpers", () => {
     expect(rulesFn(input)).toBe(3);
   });
 
-  test("OMIT", () => {
+  test('OMIT', () => {
     const rulesFn = ruleFactory('return OMIT("London", cities)');
     const input = {
       cities: { Denver: true, London: true, LA: true },
