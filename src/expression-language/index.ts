4de9c491057b0fd0e2b3d92f1cecb8f3309874c9
Chris Hatch <chris.hatch@hotelengine.com>
2022-10-29 06:44:44 -0500

    formatting

--- src/expression-language/index.ts
+++ src/expression-language/index.ts
@@ -35,7 +35,7 @@ import {
 const hasOwnProperty = (obj: object, key: string) =>
   Object.prototype.hasOwnProperty.call(obj, key);
 export interface FunctionOps {
-  [op: string]: (...args: ExpressionThunk[]) => ExpressionValue;
+  [op: string]: (...args: ExpressionThunk[]) => ExpressionValue
 }
 
 export const assignmentOperators = ['=', '+=', '-=', '*=', '/=', '??='];
@@ -85,7 +85,7 @@ type Callable = (...args: ExpressionArray<ExpressionThunk>) => ExpressionValue;
 
 type TermSetterFunction = (keyPath: string, value: ExpressionValue) => any;
 
-export const ruleExpressionLanguage = function (
+export const ruleExpressionLanguage = function(
   termDelegate: TermDelegate,
   termTypeDelegate?: TermTyper,
   termSetter?: TermSetterFunction
@@ -540,7 +540,7 @@ export const ruleExpressionLanguage = function (
       },
     },
     // @ts-expect-error
-    termDelegate: function (term: string) {
+    termDelegate: function(term: string) {
       const numVal = parseFloat(term);
       if (Number.isNaN(numVal)) {
         switch (term.toUpperCase()) {
@@ -582,7 +582,7 @@ export const ruleExpressionLanguage = function (
       }
     },
 
-    termTyper: function (term: string): TermType {
+    termTyper: function(term: string): TermType {
       const numVal = parseFloat(term);
 
       if (Number.isNaN(numVal)) {
--- src/index.ts
+++ src/index.ts
@@ -17,29 +17,29 @@ const serialize = (data: unknown) =>
   data !== null && typeof data === 'object' ? JSON.stringify(data) : data;
 
 interface RuleMachineOptions {
-  trace?: boolean;
-  ignoreMissingKeys?: boolean;
+  trace?: boolean
+  ignoreMissingKeys?: boolean
 }
 
 interface TraceRow {
-  startTime?: number;
-  runTime?: number;
+  startTime?: number
+  runTime?: number
 
-  operation: string;
-  rule?: Rule;
-  input?: any;
-  result?: any;
-  stepRow?: number;
-  stepCount?: number;
-  lhs?: string;
-  value?: ExpressionValue;
-  error?: any;
-  [key: string]: unknown;
+  operation: string
+  rule?: Rule
+  input?: any
+  result?: any
+  stepRow?: number
+  stepCount?: number
+  lhs?: string
+  value?: ExpressionValue
+  error?: any
+  [key: string]: unknown
 }
 
 export function ruleFactory<
   TInput extends {
-    [k: string]: string | boolean | number | null | undefined | TInput;
+    [k: string]: string | boolean | number | null | undefined | TInput
   } = any
 >(
   rules: Rule,
@@ -332,10 +332,10 @@ export function ruleFactory<
       rule,
       ignoreMissingKeys = false,
     }: {
-      stepRow: number;
-      input: TInput;
-      rule: string | string[] | Rule;
-      ignoreMissingKeys?: boolean;
+      stepRow: number
+      input: TInput
+      rule: string | string[] | Rule
+      ignoreMissingKeys?: boolean
     }):
       | string
       | boolean
@@ -404,7 +404,7 @@ export function ruleFactory<
 
 export function extractValueOrLiteral<
   TInput extends {
-    [k: string]: string | boolean | number | null | undefined | TInput;
+    [k: string]: string | boolean | number | null | undefined | TInput
   } = any
 >(
   input: TInput,
@@ -433,18 +433,18 @@ export function extractValueOrLiteral<
 export type Rule =
   | string
   | {
-      if: Rule;
-      then: Rule;
-      else?: Rule;
-    }
+    if: Rule
+    then: Rule
+    else?: Rule
+  }
   | {
-      and: Rule[];
-    }
+    and: Rule[]
+  }
   | {
-      or: Rule[];
-    }
+    or: Rule[]
+  }
   | {
-      return: Rule;
-    }
-  | { try: Rule; catch: Rule }
+    return: Rule
+  }
+  | { try: Rule, catch: Rule }
   | Rule[];
